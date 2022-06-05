edd364b2dd8f0cdfd3ac9b6ccb0fd9328b4a22ad
i-naeem <inaeem@protonmail.com>
2022-06-05 08:25:44 +0500

    feat: :sparkles: add header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-import { ChakraProvider, Text, theme } from '@chakra-ui/react';
+import {
+  Box,
+  Button,
+  ChakraProvider,
+  Heading,
+  HStack,
+  theme,
+} from '@chakra-ui/react';
 import React from 'react';
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
-      <Text>Hello World.</Text>
+      <Box as="header" height="75px" boxShadow="base">
+        <HStack p="2" justify="space-between" align="center" height="100%">
+          <Heading>Google</Heading>
+
+          <Button
+            w="180px"
+            bgColor="#1A73E8"
+            colorScheme="blue"
+            borderRadius="base"
+          >
+            Go to FGallery
+          </Button>
+        </HStack>
+      </Box>
     </ChakraProvider>
   );
 }
