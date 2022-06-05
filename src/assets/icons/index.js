62091791111c3f01032a08052390167de2e6f2cc
i-naeem <inaeem@protonmail.com>
2022-06-05 12:04:33 +0500

    :rotating_light: fix `FaCogs is defined but never used`

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -1,5 +1,5 @@
 import { Icon } from '@chakra-ui/react';
-import { FaCog, FaCogs, FaSearch, FaUpload } from 'react-icons/fa';
+import { FaCog, FaSearch, FaUpload } from 'react-icons/fa';
 
 export const SearchIcon = props => <Icon as={FaSearch} {...props} />;
 export const UploadIcon = props => <Icon as={FaUpload} {...props} />;
