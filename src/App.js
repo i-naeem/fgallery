185a466312561c79bd9d2580bad0f55f3501b8c3
i-naeem <inaeem@protonmail.com>
2022-06-05 11:24:15 +0500

    :construction: add dashboard route

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import { ChakraProvider, theme } from '@chakra-ui/react';
+import Dashboard from './routes/dashboard/Dashboard';
+import Login from './routes/Login';
 import Home from './routes/Home';
 import React from 'react';
-import Login from './routes/Login';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/app" element={<Dashboard />} />
           <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
       </BrowserRouter>
