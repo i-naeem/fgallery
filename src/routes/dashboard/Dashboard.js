aa35c3db61a821bdf100819543b1a5a08b43992b
i-naeem <inaeem@protonmail.com>
2022-06-05 11:24:27 +0500

    :construction: add dashboard route

---
+++ src/routes/dashboard/Dashboard.js
@@ -0,0 +1,13 @@
+import { Box, Text } from '@chakra-ui/react';
+
+const Dashboard = props => {
+  return (
+    <Box>
+      <Box height="calc(90vh - 75px)">
+        <Text>Hello World</Text>
+      </Box>
+    </Box>
+  );
+};
+
+export default Dashboard;
