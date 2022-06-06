9bb8a63f85654c73ecb5778d59ae14710cdcf74f
i-naeem <inaeem@protonmail.com>
2022-06-06 16:31:07 +0500

    :sparkles: add isSelected

--- src/components/PhotoItem.js
+++ src/components/PhotoItem.js
@@ -1,10 +1,26 @@
 import { Box, Checkbox, Image } from '@chakra-ui/react';
+import { useState } from 'react';
 
 const PhotoItem = ({ img }) => {
+  const [isSelected, setIsSelected] = useState(false);
   return (
-    <Box role="button" pos="relative">
-      <Box pos="absolute" bgColor="blackAlpha.200" p="2" width="100%">
-        <Checkbox />
+    <Box
+      role="button"
+      pos="relative"
+      _hover={{ '.select': { display: 'block' } }}
+    >
+      <Box
+        pos="absolute"
+        bgColor="blackAlpha.200"
+        p="2"
+        width="100%"
+        className="select"
+        sx={{ display: isSelected ? 'block' : 'none' }}
+      >
+        <Checkbox
+          value={isSelected}
+          onChange={() => setIsSelected(!isSelected)}
+        />
       </Box>
       <Image src={img.src} alt={img.alt} width="100%" />
     </Box>
